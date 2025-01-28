2b6af37c292a3f1c50c774dc2b36cb97ca3b45b7
yk1313-glitch <w7126945@gmail.com>
2025-01-28 11:12:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const bot1Prompt = JSON.parse(
-   fs.readFileSync('./prompts/adobe.json', 'utf8')
+   fs.readFileSync('./prompts/punk.json', 'utf8')
 );
 
 const app = express();
 const PORT = 4000;
 const TOKEN = process.env.TOKEN;
 
-const allowedOrigins = ['https://adobecoin.io'];
+const allowedOrigins = ['https://deeppunk.ink'];
 
 app.use(
    cors({
